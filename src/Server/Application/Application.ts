c33ea5c967e1b2997ff3b1eb580fe12c8223722c
Levente Löki <leventeloki@gmail.com>
2018-03-31 21:06:13 +0200

    Koa

--- src/Server/Application/Application.ts
+++ src/Server/Application/Application.ts
@@ -1,5 +1,7 @@
 import DataStore from '../../Entity/DataStore'
 import MainPageInfoLoader from '../Loader/MainPageInfoLoader'
+import * as Koa from 'koa'
+import * as Router from 'koa-router'
 
 export default class Application {
 
@@ -13,6 +15,13 @@ export default class Application {
         this.mainPageInfoLoader.load(this.dataStore).then((store) => {
             this.dataStore = store
         })
+        const koa = new Koa()
+        const router = new Router()
+        router.get('/api/v1/data', async (ctx, next) => {
+            ctx.body = 'XXX'
+        })
+        koa.use(router.routes()).use(router.allowedMethods())
+        koa.listen(6810)
     }
 
 }
